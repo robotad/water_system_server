77e6c9fffcd3ee3723a10f8a46a1eba521fb89e9
robotad <adamosdas@gmail.com>
2018-05-18 03:46:50 +0000

    Implement serial in -> post data cycle

--- water-pi/pi.py
+++ water-pi/pi.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 import requests
+import serial
+import sys
 
-req = requests.get("https://18.206.145.189:8000", cert=("pi.crt", "pi.key"), verify="ca.crt")
 
+ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', timeout=5)
+
+while True:
+    print('Serial in: %s' % (ser.readline()))
+    req = requests.post("https://18.206.145.189:8000", json="{\"test\":\"test\"}", cert=("pi.crt", "pi.key"), verify="ca.crt")
+    sys.stdout.write('Response(%s): %s\n' % (req.status_code, req.text))
